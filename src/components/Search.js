88c57ab631e8716a0a6df0b2e69e91fd2a1ece01
jump3rhood <mjm13121@gmail.com>
2022-01-28 15:23:02 +0530

    fix eslint warning using another useEffect

--- src/components/Search.js
+++ src/components/Search.js
@@ -2,7 +2,18 @@ import React, { useState, useEffect } from "react";
 import axios from "axios";
 const Search = () => {
   const [term, setTerm] = useState("react");
+  const [debouncedTerm, setDebouncedTerm] = useState(term);
   const [results, setResults] = useState([]);
+
+  useEffect(() => {
+    const timeoutId = setTimeout(() => {
+      if (term) setDebouncedTerm(term);
+    }, 500);
+
+    return () => {
+      clearTimeout(timeoutId);
+    };
+  }, [term]);
 
   useEffect(() => {
     const search = async () => {
@@ -12,24 +23,13 @@ const Search = () => {
           list: "search",
           origin: "*",
           format: "json",
-          srsearch: term,
+          srsearch: debouncedTerm,
         },
       });
       setResults(data.query.search);
     };
-    let timeoutId;
-    if (term && !results.length) {
-      search();
-    } else {
-      timeoutId = setTimeout(() => {
-        if (term) search();
-      }, 500);
-    }
-    return () => {
-      clearTimeout(timeoutId);
-    };
-  }, [term]);
-
+    search();
+  }, [debouncedTerm]);
   const renderedResults = results.map((result) => {
     return (
       <div className="item" key={result.pageid}>
