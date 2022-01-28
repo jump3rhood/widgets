70d9eb9f90baada8d517c8880923f99062e82818
jump3rhood <mjm13121@gmail.com>
2022-01-28 15:00:53 +0530

    debounce search function and add cleanup in useEffect

--- src/components/Search.js
+++ src/components/Search.js
@@ -4,7 +4,6 @@ const Search = () => {
   const [term, setTerm] = useState("react");
   const [results, setResults] = useState([]);
 
-  console.log(results);
   useEffect(() => {
     const search = async () => {
       const { data } = await axios.get("https://en.wikipedia.org/w/api.php", {
@@ -18,11 +17,30 @@ const Search = () => {
       });
       setResults(data.query.search);
     };
-    if (term) search();
+    let timeoutId;
+    if (term && !results.length) {
+      search();
+    } else {
+      timeoutId = setTimeout(() => {
+        if (term) search();
+      }, 500);
+    }
+    return () => {
+      clearTimeout(timeoutId);
+    };
   }, [term]);
+
   const renderedResults = results.map((result) => {
     return (
       <div className="item" key={result.pageid}>
+        <div className="right floated content">
+          <a
+            className="ui button"
+            href={`https://en.wikipedia.org?curid=${result.pageid}`}
+          >
+            Go
+          </a>
+        </div>
         <div className="content">
           <div className="header">{result.title}</div>
           <span dangerouslySetInnerHTML={{ __html: result.snippet }}></span>
